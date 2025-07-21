e9508f5cc0d3982642b0263f83cc9b4b1d1d542a
Ndam Njogho Fuh <ndamnjogho6@gmail.com>
2025-07-21 19:00:12 -0400

    updated task definition file

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
+import json
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_secret():
-    secret = os.getenv("MY_SECRET_VALUE")
-    if secret:
-        return jsonify({"message": "Secret pulled successfully", "secret": secret})
-    else:
-        return jsonify({"error": "Secret not found"}), 500
+    secret_str = os.getenv("MY_SECRET_VALUE")
+    try:
+        secret_dict = json.loads(secret_str)
+        return jsonify({
+            "message": "Secret pulled successfully",
+            "secret": secret_dict.get("MY_SECRET_VALUE")
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
