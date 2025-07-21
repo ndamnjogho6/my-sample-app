d1b0f29ac544febf89012175d9f9cba358f132e1
Ndam Njogho Fuh <ndamnjogho6@gmail.com>
2025-07-21 19:13:35 -0400

    updated task definition file

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_secret():
+    # The secret is injected into the environment as a JSON string
     secret_str = os.getenv("MY_SECRET_VALUE")
     try:
+        # Parse the string to a dictionary
         secret_dict = json.loads(secret_str)
+        # Return only the value
         return jsonify({
             "message": "Secret pulled successfully",
             "secret": secret_dict.get("MY_SECRET_VALUE")
