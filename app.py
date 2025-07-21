1a36f63d9737928786799a93d215437024393be3
Ndam Njogho Fuh <ndamnjogho6@gmail.com>
2025-07-21 18:56:46 -0400

    updated task definition file

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import boto3
 import os
 from flask import Flask, jsonify
 
@@ -6,18 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_secret():
-    secret_name = os.environ.get("MY_SECRET_VALUE")
-    region_name = os.environ.get("AWS_REGION", "us-east-1")
-
-    client = boto3.client('secretsmanager', region_name=region_name)
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-        secret = get_secret_value_response['SecretString']
+    secret = os.getenv("MY_SECRET_VALUE")
+    if secret:
         return jsonify({"message": "Secret pulled successfully", "secret": secret})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    else:
+        return jsonify({"error": "Secret not found"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
-# Minor update to trigger GitHub Actions pipeline for deployment
